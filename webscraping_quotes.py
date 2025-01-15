5f816880fd1562fda18d8e6fc10b9d9a69a89ded
Silindokuhle Sambesiwe Kaspile <109023953+SambesiweSlii@users.noreply.github.com>
2025-01-15 18:33:01 +0200

    Add files via upload

--- webscraping_quotes.py
+++ webscraping_quotes.py
@@ -8,13 +8,20 @@ Created on Tue Jan 14 12:51:25 2025
 from bs4 import BeautifulSoup
 import requests
 
-# requesting target website and storing it as a variable
+# requesting connection to target website and storing it as a variable
 page_to_scrape = requests.get('http://quotes.toscrape.com')
+
+# here we are parsing the html contents of the website.
 soup = BeautifulSoup(page_to_scrape.text, 'html.parser')
 
+# the variables 'quotes' and 'authors' are used to store the information we need
+# from the website. Which is the quote and the author.
 quotes = soup.find_all('span', attrs={'class':'text'})
 authors = soup.find_all('small', attrs={'class':'author'})
 
+# since there are multiple quotes and authors.
+# we use the for loop to loop through all of them allowing us to scrape all of
+# the quotes along with their authors.
 for quote, author in zip (quotes, authors):
-    print(quote.text + ' -- ' + author.text)
+    print(quote.text + ' -- ' + author.text + '\n')
     
